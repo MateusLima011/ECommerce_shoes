2c7095cf4eee74eb8183b98d0a48020a0c73d618
mateu <mateuslima.ml76@gmail.com>
2023-07-25 15:29:08 -0300

    Initial commit

---
+++ app/src/main/java/com/example/ecommerce_shoes/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.ecommerce_shoes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
