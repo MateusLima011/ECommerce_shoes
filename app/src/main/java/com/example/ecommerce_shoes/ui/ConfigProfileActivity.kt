89b77d547d5bd1ce3d3dce8a29a86221da9a72f4
mateu <mateuslima.ml76@gmail.com>
2023-08-25 18:52:35 -0300

    Trabalhando com a ImagePicker API para seleção de imagem

--- app/src/main/java/com/example/ecommerce_shoes/ui/ConfigProfileActivity.kt
+++ app/src/main/java/com/example/ecommerce_shoes/ui/ConfigProfileActivity.kt
@@ -1,16 +1,26 @@
 package com.example.ecommerce_shoes.ui
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
 import android.os.Handler
 import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.Toast
+import com.blankj.utilcode.util.ColorUtils
+import com.blankj.utilcode.util.KeyboardUtils
+import com.bumptech.glide.Glide
 import com.example.ecommerce_shoes.R
 import com.example.ecommerce_shoes.databinding.ContentConfigProfileBinding
 import com.example.ecommerce_shoes.domain.User
 import com.example.ecommerce_shoes.util.validate
+import com.github.dhaval2404.imagepicker.ImagePicker
 
-class ConfigProfileActivity : FormActivity<ContentConfigProfileBinding>() {
+
+class ConfigProfileActivity : FormActivity<ContentConfigProfileBinding>(),
+    KeyboardUtils.OnSoftInputChangedListener {
+
     override fun createBinding(layoutInflater: LayoutInflater) =
         ContentConfigProfileBinding.inflate(layoutInflater)
 
@@ -20,17 +30,18 @@ class ConfigProfileActivity : FormActivity<ContentConfigProfileBinding>() {
 
         binding.etNameProfile.validate({ it.length > 1 }, getString(R.string.invalid_name))
         binding.etNameProfile.setOnEditorActionListener(this)
-        binding.ivProfile.setOnClickListener { callGallery() }
+        binding.rivProfile.setOnClickListener { callGallery() }
         binding.btSendProfile.setOnClickListener { mainAction() }
 
         val user = intent.getParcelableExtra<User>(User.KEY)
         if (user != null) {
             binding.etNameProfile.setText(user.name)
         }
+        binding.rivProfile.setImageResource(user?.image ?: R.drawable.profile_hint)
     }
 
     override fun blockFields(status: Boolean) {
-        binding.ivProfile.isEnabled = !status
+        binding.rivProfile.isEnabled = !status
         binding.etNameProfile.isEnabled = !status
         binding.btSendProfile.isEnabled = !status
     }
@@ -66,7 +77,27 @@ class ConfigProfileActivity : FormActivity<ContentConfigProfileBinding>() {
     }
 
     private fun callGallery() {
-        Toast.makeText(this, "TODO", Toast.LENGTH_SHORT)
-            .show()
+        ImagePicker.with(this)
+            .crop()
+            .compress(1024)
+            .maxResultSize(1080, 1080)
+            .start()
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK && requestCode == ImagePicker.REQUEST_CODE) {
+            val imageUri: Uri = data?.data ?: return
+
+            Glide.with(this)
+                .load(imageUri)
+                .into(binding.rivProfile)
+        }
+    }
+
+    override fun onSoftInputChanged(height: Int) {
+        TODO("Not yet implemented")
+    }
+
 }
